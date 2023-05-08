fc002308e3c9b611a357045e717e370105eaceca
nadine-tim <nadinetimeiko@gmail.com>
2023-05-08 23:55:23 +0300

    done hw-6

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 import os
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class Video:
@@ -9,17 +10,24 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id: str) -> None:
+        self.video_id = video_id
         """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
-
-        video = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                           id=video_id
-                                           ).execute()
-
-        self.vido_id = video_id
-        self.title = video['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/channel/{video_id}"
-        self.view_count = video['items'][0]['statistics']['viewCount']
-        self.like_count = video['items'][0]['statistics']['likeCount']
+        try:
+            video = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=video_id
+                                               ).execute()
+            if len(video['items']) == 0:
+                raise HttpError
+        except:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+        else:
+            self.title = video['items'][0]['snippet']['title']
+            self.url = f"https://www.youtube.com/channel/{video_id}"
+            self.view_count = video['items'][0]['statistics']['viewCount']
+            self.like_count = video['items'][0]['statistics']['likeCount']
 
     def __str__(self):
         return f'{self.title}'
